b560dc45f749d5d6caa86c39ef16b6d09166e143
Olesya <vaska9990@yandex.ru>
2025-12-10 10:22:50 +0300

    added scope.cancel()

--- app/src/main/java/com/example/installingapps/data/AppsDetailsRepository.kt
+++ app/src/main/java/com/example/installingapps/data/AppsDetailsRepository.kt
@@ -5,6 +5,7 @@ import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
+import kotlinx.coroutines.cancel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import java.io.File
@@ -37,6 +38,7 @@ class AppsDetailsRepository @Inject constructor(
                 checksum = checksum.await(),
             )
 
+        scope.cancel()
         return flow {
             emit(app)
         }
